93b6947b831803b57052fc2ac2cce5f080cf2102
chut-th <chutchawin.th@outlook.com>
2024-12-01 10:24:20 +0700

    Initial commit

---
+++ environment/npr/resource-group/main.tf
@@ -0,0 +1,19 @@
+module "resource_group_nw_01" {
+  source  = "chut-th/resource-group/azurerm"
+  version = "1.0.3"
+  company = "ct"
+  environment = "npr"
+  location = "southeastasia"
+  project = "shinw"
+  running_number = "01"
+}
+
+module "resource_group_sta_01" {
+  source  = "chut-th/resource-group/azurerm"
+  version = "1.0.3"
+  company = "ct"
+  environment = "npr"
+  location = "southeastasia"
+  project = "shista"
+  running_number = "01"
+}
